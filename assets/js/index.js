7d8197e981d4b4fab5a4aa3b1a5997a12c5ffd72
ForKio <55454285+TFerrarah@users.noreply.github.com>
2022-03-15 20:33:29 +0100

    Got javascript working on the first try yay

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,25 @@
 const body = document.getElementsByTagName('body')[0];
-const colors = document.querySelectorAll('input[name=color]');
 
-console.log(colors);
+function changeBodyColor(c) {
+    body.style.background = c;
+}
+
+const form = document.querySelector('form');
+const customHex = document.querySelector('input[name=hex]')
+customHex.value = '';
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault() //Prevents default form submit behaviour aka prevents page reload
+    const selectedColor = document.querySelector('input[name=color]:checked');
+
+    //Check if user has typed custom hex
+    if (customHex.value) {
+        //Check if hex value is valid
+        if (/^#([0-9A-F]{3}){1,2}$/i.test('#'+customHex.value)) { // Source: https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation
+            changeBodyColor('#' + customHex.value);
+        }
+    } else {
+        console.log(selectedColor);
+        changeBodyColor('var(--'+selectedColor.id+')');
+    }
+})
