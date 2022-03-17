6a81562ffe1300d60db05300880856474d1f2f6b
ForKio <55454285+TFerrarah@users.noreply.github.com>
2022-03-17 21:32:27 +0100

    Code cleanup + improved comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,27 @@
-const body = document.getElementsByTagName('body')[0];
+const body = document.querySelector('body');
+const form = document.querySelector('form');
+const customHex = document.querySelector('input[name=hex]')
+customHex.value = ''; //Clear input
 
 function changeBodyColor(c) {
     body.style.background = c;
 }
 
-const form = document.querySelector('form');
-const customHex = document.querySelector('input[name=hex]')
-customHex.value = '';
-
 form.addEventListener('submit', (e) =>{
     e.preventDefault() //Prevents default form submit behaviour aka prevents page reload
-    const selectedColor = document.querySelector('input[name=color]:checked');
-
+    const selectedColor = document.querySelector('input[name=color]:checked'); // this indicates the selected radio buttons
     //Check if user has typed custom hex
     if (customHex.value) {
-        //Check if hex value is valid
-        if (/^#([0-9A-F]{3}){1,2}$/i.test('#'+customHex.value)) { // Source: https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation
+        //Check if hex value is valid using RegEx expression
+        /*  Check if string contains numbers from 0 to 9 [0-9]
+            or letters from A to F (ignoring casing) [A-F]
+            and it's length is 3 [ {3} ]
+            and it's repeated 1 or 2 times [ {1,2} ]
+        */
+        if (/^([0-9A-F]{3}){1,2}$/i.test(customHex.value)) { // Source: https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation
             changeBodyColor('#' + customHex.value);
         }
-    } else {
-        console.log(selectedColor);
+    } else { //Else check the radio buttons
         changeBodyColor('var(--'+selectedColor.id+')');
     }
 })
